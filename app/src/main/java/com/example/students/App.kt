cb97d8c8931ed23c0b3e376820fb5e8093ae280a
rafael <toizhanov77@gmail.com>
2023-06-02 15:36:50 +0600

    Merge from dev

--- app/src/main/java/com/example/students/App.kt
+++ app/src/main/java/com/example/students/App.kt
@@ -36,6 +36,7 @@ class App : Application() {
                 otpModule,
                 feedPageModule,
                 mapModule,
+                matchingModule,
                 chatModule
             )
         }
